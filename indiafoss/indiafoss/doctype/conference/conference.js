37957121b65c00035b6356b77fc0305a3617c677
Shridhar <shridhar.p@zerodha.com>
2022-09-16 18:35:48 +0530

    feat: display note on conference doctype

--- indiafoss/indiafoss/doctype/conference/conference.js
+++ indiafoss/indiafoss/doctype/conference/conference.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Conference', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.dashboard.add_comment("Upload all inages as public, as private files fails to load on website", "red", true)
+	}
 });
