681d62ae958dfba90ff301973e2df69b3343f1cf
Abhinav Raut <abhinavrautcs@gmail.com>
2023-01-14 16:05:17 +0530

    feat: razorpay payments integration

---
+++ indiafoss/indiafoss/doctype/conference_payment/test_conference_payment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, shridhar.p@zerodha.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestConferencePayment(FrappeTestCase):
+    pass
