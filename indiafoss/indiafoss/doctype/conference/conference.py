da3ed8255862a25946e742b8ece019b736a0b3c0
Abhinav Raut <abhinavrautcs@gmail.com>
2023-01-26 17:05:30 +0530

    fix: book tickets page - show errors when no ticket is selected - show err when conference doesn't exist

--- indiafoss/indiafoss/doctype/conference/conference.py
+++ indiafoss/indiafoss/doctype/conference/conference.py
@@ -11,9 +11,18 @@ class Conference(Document):
 
 @frappe.whitelist(allow_guest=True)
 def get_ticket_data(doc_name):
+    exists = frappe.db.exists("Conference", doc_name)
+    if not exists:
+        return {
+            'student_ticket_price': 0,
+            'general_ticket_price': 0,
+            'max_tickets': 0,
+            'tickets_booked': 0,
+        }
+
     st_price, gen_price, max_tickets, tickets_booked = frappe.get_value(
         "Conference",
-        doc_name,
+        exists,
         [
             "student_ticket_price",
             "general_ticket_price",
