dff44763608fd00ac11bbb68d9128e95b43837f1
Abhinav Raut <abhinavrautcs@gmail.com>
2023-03-05 21:21:30 +0530

    move payment capture logic to hook

--- indiafoss/indiafoss/doctype/conference_payment/conference_payment.py
+++ indiafoss/indiafoss/doctype/conference_payment/conference_payment.py
@@ -6,8 +6,21 @@ from frappe.model.document import Document
 
 
 class ConferencePayment(Document):
-
     def on_payment_authorized(self, payment_status):
         if payment_status == "Authorized":
             self.payment_captured = 1
+
+            # increase tickets booked count
+            tickets_booked = frappe.db.get_value(
+                "Conference", self.conference, "tickets_booked"
+            )
+
+            frappe.db.set_value(
+                "Conference",
+                self.conference,
+                "tickets_booked",
+                tickets_booked
+                + self.student_tickets
+                + self.general_tickets,
+            )
             self.save(ignore_permissions=True)
