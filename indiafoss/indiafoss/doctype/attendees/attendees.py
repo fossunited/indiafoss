e51b61cfe18374d7ca92d9a21f51b98375228322
RiyaMathew-11 <riyapmathew2000@gmail.com>
2024-01-17 21:30:29 +0530

    add: conference attendee childtable

---
+++ indiafoss/indiafoss/doctype/attendees/attendees.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, shridhar.p@zerodha.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Attendees(Document):
+	pass
